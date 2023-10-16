Small refactoring. Fixed calculation of added parameters. Added more entries to tables for ML prediction

stockmarket_bot/coinbase_api/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+import pandas as pd
+from torch import no_grad, tensor, float32
+import numpy as np
+import xgboost as xgb
 
 class Cryptocurrency(models.Model):
     base_display_symbol = models.CharField(max_length=255)  # For storing the symbol of the base (e.g. BTC)
@@ -24,8 +28,8 @@ class AbstractOHLCV(models.Model):
     ema = models.FloatField(null=True)
     rsi = models.FloatField(null=True)
     macd = models.FloatField(null=True)
-    bollinger_low = models.FloatField(null=True)
     bollinger_high = models.FloatField(null=True)
+    bollinger_low = models.FloatField(null=True)
     vmap = models.FloatField(null=True)
     percentage_returns = models.FloatField(null=True)
     log_returns = models.FloatField(null=True)
@@ -41,6 +45,64 @@ class AbstractOHLCV(models.Model):
             models.Index(fields=['timestamp', 'close']),   # Composite index example
             # ... add more indexes as needed ...
         ]
+
+    @staticmethod
+    def queryset_to_lstm_dataframe(queryset, seq_length=100):
+        features = ['volume', 'sma', 'ema', 'rsi', 'macd', 'bollinger_high', 'bollinger_low', 'vmap', 'percentage_returns', 'log_returns']
+        # Convert the queryset to a list of dictionaries
+        data_dict_list = queryset.values()
+        # print(data_dict_list)
+        dataframe = pd.DataFrame.from_records(data_dict_list, index='timestamp')
+        dataframe.drop(columns=['id'], inplace=True)
+        prices = dataframe[features].values
+        # print(prices[-5:])
+        X = []
+        # for i in range(len(prices) - seq_length):
+        X.append(prices[:, :len(features)])
+            # y_values = [prices[i+seq_length, len(features)+target_idx] for target_idx in range(len(targets))]
+            # y.append(y_values)
+        # Convert the list of dictionaries to a DataFrame
+        tensor_data = tensor(X, dtype=float32)
+        return tensor_data
+    
+    @staticmethod
+    def queryset_to_xgboost_dataframe(queryset):
+        def check_timestamp(ts):
+            return len(str(ts)) == 13
+        features = ['volume', 'sma', 'ema', 'rsi', 'macd', 'bollinger_high', 'bollinger_low', 'vmap', 'percentage_returns', 'log_returns']
+        # Convert the queryset to a list of dictionaries
+        data_dict_list = queryset.values()
+        # print(data_dict_list)
+        dataframe = pd.DataFrame.from_records(data_dict_list)
+        dataframe.drop(columns=['id'], inplace=True)
+        dataframe['timestamp'] = dataframe['timestamp'].apply(lambda x: x//1000 if check_timestamp(x) else x)
+        dataframe['Datetime'] = pd.to_datetime(dataframe['timestamp'], unit='s')
+        dataframe['Hour'] = dataframe['Datetime'].dt.hour
+        dataframe['Day_of_Week'] = dataframe['Datetime'].dt.dayofweek  # Monday=0, Sunday=6
+        dataframe['Day_of_Month'] = dataframe['Datetime'].dt.day
+        dataframe['Month'] = dataframe['Datetime'].dt.month
+        dataframe['Year'] = dataframe['Datetime'].dt.year
+        dataframe['Is_Weekend'] = (dataframe['Day_of_Week'] >= 5).astype(int)  # 1 for weekend, 0 for weekdays
+        # Updating the features list
+        features_extended = features + ['Hour', 'Day_of_Week', 'Day_of_Month', 'Month', 'Year', 'Is_Weekend']
+        #  ('MACD', 0.045680176),
+        #  ('SMA', 0.039496846),
+        #  ('Day_of_Week', 0.038991235),
+        #  ('Day_of_Month', 0.038741197),
+        #  ('Hour', 0.03847502),
+        #  ('Log_Returns', 0.0),
+        #  ('Is_Weekend', 0.0)]
+        drop_features = ['macd', 'sma', 'Day_of_Week', 'Day_of_Month', 'Hour', 'log_returns', 'Is_Weekend']
+        features_extended = [feature for feature in features_extended if feature not in drop_features]
+        print(f'len features: {len(features)}; len features extended: {len(features_extended)}')
+        prices = dataframe[features_extended].values
+        # X = prices[:, :-3]  # Features
+        # X = np.array(prices).reshape((1, -1))
+        X = prices
+        tmp = xgb.DMatrix(X)
+        # print(prices.tail())
+        return tmp
+        
 
 class Bitcoin(AbstractOHLCV):
     symbol = "BTC"

stockmarket_bot/coinbase_api/serializers.py
@@ -8,25 +8,60 @@ class CryptocurrencySerializer(serializers.ModelSerializer):
         model = Cryptocurrency
         fields = ['id', 'base_display_symbol', 'quote_display_symbol','product_id', 'trading_indicator']
 
-from rest_framework import serializers
 
-crypto_fields = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
 
-# class AbstractOHLCVSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         fields = crypto_fields
+crypto_fields = [
+    'timestamp', 
+    'close', 
+    'volume', 
+    'sma', 
+    'ema', 
+    'rsi', 
+    'macd', 
+    'bollinger_high', 
+    'bollinger_low', 
+    'vmap',
+    'percentage_returns',
+    'log_returns',
+    ]
+
+extra_kwargs = {
+    'sma': {'required': False},
+    'ema': {'required': False},
+    'rsi': {'required': False},
+    'macd': {'required': False},
+    'bollinger_high': {'required': False},
+    'bollinger_low': {'required': False},
+    'vmap': {'required': False},
+    'percentage_returns': {'required': False},
+    'log_returns': {'required': False},
+    } 
 
 class BitcoinSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bitcoin
         fields=crypto_fields
+        extra_kwargs = extra_kwargs
 
 class EthereumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ethereum
         fields=crypto_fields
+        extra_kwargs = extra_kwargs
 
 class PolkadotSerializer(serializers.ModelSerializer):
     class Meta:
         model = Polkadot
         fields=crypto_fields
+        extra_kwargs = extra_kwargs
+
+import json
+import numpy as np
+from django.core.serializers.json import DjangoJSONEncoder
+
+class NaNJSONEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        print(f'obj: {obj}')
+        if isinstance(obj, float) and np.isnan(obj):
+            return None  # or use the string "NaN"
+        return super().default(obj)

stockmarket_bot/coinbase_api/utilities/ml_utils.py
@@ -1,2 +1,125 @@
-def prepare_lstm_data():
-    pass
+
+from datetime import datetime, timedelta, timezone
+from django.utils.timezone import make_aware
+import numpy as np
+import pandas as pd
+import ta
+from django.db.models import Sum, F, FloatField, ExpressionWrapper
+
+def calculate_vmap(queryset):
+    aggregated_data = queryset.aggregate(
+        total_volume=Sum('volume'),
+        total_value=Sum(
+            ExpressionWrapper(
+                ((F('open') + F('close') + F('high')) / 3) * F('volume'),
+                output_field=FloatField()
+            )
+        )
+    )
+    if aggregated_data['total_volume'] != 0:
+        vmap = aggregated_data['total_value'] / aggregated_data['total_volume']
+    else:
+        vmap = 0
+    return vmap
+
+def calculate_percentage_returns(current_item, previous_item):
+    if previous_item is None:
+        return None
+    if previous_item.close == 0:
+        return None
+    else:
+        return (current_item.close - previous_item.close) / (previous_item.close)
+    
+def calculate_log_returns(current_item, previous_item):
+    if previous_item is None:
+        return None
+    if abs(current_item.close - previous_item.close) < 0.000001:
+        return None
+    else:
+        # print(f'current item: {current_item.close}, previous_item: {previous_item.close}')
+        return np.log(current_item.close / previous_item.close)
+    
+def calculate_shifted_value(item, last_168_data, index):
+    try:
+        return last_168_data[index].close > item.close
+    except IndexError:
+        return None
+    
+def check_nan(entry):
+    '''
+    @param entry: any number
+    returns None if entry suffices np.isnan, else entry
+    '''
+    return entry if not np.isnan(entry) else None
+
+def add_calculated_parameters(crypto_model):
+    sequence_length = 50
+    rsi_length = 14
+    bollinger_length = 20
+    all_data = crypto_model.objects.all()
+    previous_item = None
+    for item in all_data:
+        data_day_of_item = crypto_model.objects.filter(timestamp__gte=item.timestamp.replace(hour=0, minute=0, second=0), timestamp__lte=item.timestamp.replace(hour=23, minute=59, second=59)).order_by('-timestamp')
+        # if data_today.count() != 0:
+        hours_ago = make_aware(datetime.utcnow().replace(hour=0, minute=0, second=0) - timedelta(hours=168))
+        last_168_data = crypto_model.objects.filter(timestamp__gte=hours_ago).order_by('timestamp')
+        data_sequence_length = crypto_model.objects.filter(timestamp__lte=item.timestamp).order_by('-timestamp')[:sequence_length]
+        close = list(data_sequence_length.values_list('close', flat=True))
+        close.append(item.close)
+        close = pd.Series(close)
+        close_bollinger = close[-bollinger_length:]
+        close_rsi = close[-rsi_length:]
+        vmap = calculate_vmap(data_day_of_item)
+        percentage_returns = calculate_percentage_returns(item, previous_item)
+        log_returns = calculate_log_returns(item, previous_item)
+        close_higher_shifted_1h = calculate_shifted_value(item, last_168_data, 1)
+        close_higher_shifted_24h = calculate_shifted_value(item, last_168_data, 24)
+        close_higher_shifted_168h = calculate_shifted_value(item, last_168_data, 168)
+        sma = ta.trend.sma_indicator(close, sequence_length)
+        ema = ta.trend.ema_indicator(close, sequence_length)
+        ema_26 = ta.trend.ema_indicator(close, 26)
+        ema_12 = ta.trend.ema_indicator(close, 12)
+        macd = ema_12 if ema_12.iloc[-1] != np.nan else 0 - ema_26 if ema_26.iloc[-1]!=np.nan else 0
+        bollinger = ta.volatility.BollingerBands(close_bollinger, bollinger_length, 2)
+        bollinger_high = bollinger.bollinger_hband()
+        bollinger_low = bollinger.bollinger_lband()
+        rsi = ta.momentum.rsi(close_rsi, rsi_length)
+        item.vmap = vmap
+        item.percentage_returns = percentage_returns
+        item.log_returns = log_returns
+        item.close_higher_shifted_1h = close_higher_shifted_1h
+        item.close_higher_shifted_24h = close_higher_shifted_24h
+        item.close_higher_shifted_168h = close_higher_shifted_168h
+        item.sma = check_nan(sma.iloc[-1])
+        item.ema = check_nan(ema.iloc[-1])
+        item.macd = check_nan(macd.iloc[-1])
+        item.bollinger_high = check_nan(bollinger_high.iloc[-1])
+        item.bollinger_low = check_nan(bollinger_low.iloc[-1])
+        item.rsi = check_nan(rsi.iloc[-1])
+        item.save()
+        previous_item = item
+        # print(f'new_entry: {new_entry}')
+        # print(f'type of sma: {type(sma.iloc[-1])}')
+        # print(type(new_entry.sma))
+        # print(type(new_entry.ema))
+        # print(type(new_entry.macd))
+        # print(type(new_entry.bollinger_high))
+        # print(type(new_entry.bollinger_low))
+        # print(type(new_entry.rsi))
+        # if type(new_entry.sma) != np.float64:
+        #     print(f'sma: {new_entry.sma}')
+        # if type(new_entry.ema) != np.float64:
+        #     print(f'ema: {new_entry.ema}')
+        # if type(new_entry.macd) != np.float64:
+        #     print(f'macd: {new_entry.macd}')
+        # if type(new_entry.bollinger_high) != np.float64:
+        #     print(f'bollinger_high: {new_entry.bollinger_high}')
+        # if type(new_entry.bollinger_low) != np.float64:
+        #     print(f'bollinger_low: {new_entry.bollinger_low}')
+        # if type(new_entry.rsi) != np.float64:
+        #     print(f'rsi: {new_entry.rsi}')
+        # print(f'sma is none?: {sma.iloc[-1] == np.nan}')
+        # print(f'sma is none?: {sma.iloc[-1] == np.NAN}')
+        # print(f'sma is none?: {np.isnan(sma.iloc[-1])}')
+        # print(f'sma is "nan"?: {sma.iloc[-1] == "nan"}')
+        # break